07e9444efc2db7db118ee026780f101c58309afd
Victor <vatt849@hotmail.com>
2014-04-23 21:18:04 +0400

    first files in project

---
+++ mmsh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mmsh.Startup))]
+namespace mmsh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
